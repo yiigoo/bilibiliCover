be57639bf18757606593e949c95132605cdfa358
pactera-338 <pactera-338>
2019-03-18 11:19:59 +0800

    bilibili 封面

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,8 @@ const superagent = require('superagent')
 router.prefix('/users')
 let _body = '';
 router.get('/', function (ctx, next) {
-  url = 'https://comment.bilibili.com/recommendnew,334530000045'; //百度新闻地址
+	// url = 'http://api.bilibili.com/x/web-interface/view?aid=44695408' // 封面地址
+  url = 'https://comment.bilibili.com/recommendnew,334530000045'; // 当前视频的相关
 	
     // superagent不理解，请在文章开头的地方寻找了解superagent，点击进去理解
 	
@@ -17,6 +18,10 @@ router.get('/', function (ctx, next) {
 		}), //用cheerio解析页面数据
 		obj = {};
 		
+
+		setInterval( function(){
+			console.log( 1 )
+		}  , 1000 )
 
 		//下面类似于jquery的操作，前端的小伙伴们肯定很熟悉啦
 		console.log(  JSON.parse(sres.text) )
