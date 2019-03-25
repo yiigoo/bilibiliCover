37abf5427a1c263f95357ad07413107bac3132a4
pactera-338 <pactera-338>
2019-03-25 14:24:08 +0800

    cover 更新

--- routes/cover.js
+++ routes/cover.js
@@ -1,4 +1,5 @@
 const router = require('koa-router')()
+const superagent = require('superagent')
 
 router.get('/cover', async (ctx, next) => {
 	await ctx.render('cover', {
@@ -7,7 +8,23 @@ router.get('/cover', async (ctx, next) => {
 	})
 })
 router.get('/cover/img', async (ctx, next) => {
-	ctx.body = { msg : '成功' }
+	const url = `http://api.bilibili.com/x/web-interface/view?aid=${ctx.query.aid}`
+	let data = { data : { pic : null } }
+	function timeout() {
+		return new Promise((resolve) => {
+			superagent.get( url ).end((err, sres) => { 
+				// sres 页面获取到的数据
+				// 成功
+				console.log(JSON.parse(sres.text).code)
+				if ( JSON.parse(sres.text).code === 0 ){
+					data = JSON.parse(sres.text).data
+				}
+				resolve(JSON.parse(sres.text))
+			})
+		})
+	}
+	await timeout() 
+	ctx.body = { msg : data }
 })
 
 module.exports = router
